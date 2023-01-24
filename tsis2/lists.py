22d1a41472a29839762c4c9e011a768fa8ac414c
Polina <antrauxxxxx@mail.ru>
2023-01-24 14:45:06 +0600

    commit

---
+++ tsis2/lists.py
@@ -0,0 +1,28 @@
+#lists
+mylist = ["apple", "banana", "cherry"]
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+#duplicates
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+#listlength
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+#listitems
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+list1 = ["abc", 34, True, 40, "male"]
+
+#type()
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+#constructor
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+
